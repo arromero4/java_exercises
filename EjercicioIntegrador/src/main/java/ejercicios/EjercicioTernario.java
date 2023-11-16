f149ac1601a0a477e84ad37257a9d0b04927d0cd
arromero4 <arromero0408@gmail.com>
2023-11-15 21:19:24 -0600

    add ternary example with java

--- EjercicioIntegrador/src/main/java/ejercicios/EjercicioTernario.java
+++ EjercicioIntegrador/src/main/java/ejercicios/EjercicioTernario.java
@@ -1,7 +1,18 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class EjercicioTernario {
     public static void main(String[] args){
+        double promedio;
+        String condicionFinal;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("ingrese el promedio general del alumno: ");
+        promedio = teclado.nextDouble();
+
+        condicionFinal = promedio >= 6 ? "Aprobado" : "Reprobado";
+        System.out.println("La condicion final del alumno es: " + condicionFinal);
 
     }
 }
